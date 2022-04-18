33c0d6cc161440e50ce2df7754aa50ff02e6f9ff
Mehrad <mehradashoori456@gmail.com>
2022-04-18 18:21:54 +0430

    create custom user with forms adn signup

---
+++ accounts/forms.py
@@ -0,0 +1,20 @@
+# UserCreationForm -> Sign up
+# UserChangeForm -> Log in
+
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        # fields = UserCreationForm.Meta.fields + ('age',)
+        fields = ('username', 'age', 'email',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        # fields = UserChangeForm.Meta.fields
+        fields = ('username', 'age', 'email',)
